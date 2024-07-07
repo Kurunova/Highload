975767d11326ea3856bc462a8f8e2d4f76a8f027
Elena <lenakurunova@gmail.com>
2024-07-07 22:42:57 +0700

    hw-3 cleaning

--- init-master.sh
+++ init-master.sh
@@ -12,12 +12,21 @@
 # SHOW hba_file;
 # SHOW data_directory;
 
-# Проверка наличия флаг-файла
+# удалить строку последнюю
+# head -n -1 /var/lib/postgresql/data/pg_hba.conf > /var/lib/postgresql/data/pg_hba.conf.tmp && mv /var/lib/postgresql/data/pg_hba.conf.tmp /var/lib/postgresql/data/pg_hba.conf
+# head -n -1 /var/lib/postgresql/data/postgresql.conf > /var/lib/postgresql/data/postgresql.conf.tmp && mv /var/lib/postgresql/data/postgresql.conf.tmp /var/lib/postgresql/data/postgresql.conf
+
+# посмотреть содержимое
+# cat /var/lib/postgresql/data/postgresql.conf
+# cat /var/lib/postgresql/data/pg_hba.conf
+
+# проверка наличия флаг-файла
 if [ -f /var/lib/postgresql/data/.init_done ]; then
   echo "Initialization already done, skipping."
 else
   echo "Running initialization script for the first time..."
 
+  # логи
   echo "log_destination = 'stderr'" >> /var/lib/postgresql/data/postgresql.conf
   echo "logging_collector = on" >> /var/lib/postgresql/data/postgresql.conf
   echo "log_min_messages = debug1" >> /var/lib/postgresql/data/postgresql.conf
@@ -26,39 +35,15 @@ else
   # echo "ssl = off" >> /var/lib/postgresql/data/postgresql.conf
   echo "wal_level = replica" >> /var/lib/postgresql/data/postgresql.conf
   echo "max_wal_senders = 4" >> /var/lib/postgresql/data/postgresql.conf
-  
   echo "listen_addresses = '*'" >> /var/lib/postgresql/data/postgresql.conf
   # echo "hba_file = '/var/lib/postgresql/data/pg_hba.conf'" >> /var/lib/postgresql/data/postgresql.conf
 
-  # head -n -1 /var/lib/postgresql/data/postgresql.conf > /var/lib/postgresql/data/postgresql.conf.tmp && mv /var/lib/postgresql/data/postgresql.conf.tmp /var/lib/postgresql/data/postgresql.conf
-  
-  # cat /var/lib/postgresql/data/postgresql.conf
-  # cat /var/lib/postgresql/data/pg_hba.conf
-
-  # Настройка pg_hba.conf для репликации
-  # echo "host    replication     replicator      0.0.0.0/0      md5" >> /var/lib/postgresql/data/pg_hba.conf
-  # echo "host    replication     replicator      0.0.0.0/0      trust" >> /var/lib/postgresql/data/pg_hba.conf
+  # настройка pg_hba.conf для репликации
   echo "host    replication     replicator      0.0.0.0/0               md5" >> /var/lib/postgresql/data/pg_hba.conf
   echo "host    replication     test      0.0.0.0/0               md5" >> /var/lib/postgresql/data/pg_hba.conf
+  # чтобы можно было подключиться с бд клиента
   echo "host    all             all             0.0.0.0/0               md5" >> /var/lib/postgresql/data/pg_hba.conf
-  
-  # удалить строку последнюю
-  # head -n -1 /var/lib/postgresql/data/pg_hba.conf > /var/lib/postgresql/data/pg_hba.conf.tmp && mv /var/lib/postgresql/data/pg_hba.conf.tmp /var/lib/postgresql/data/pg_hba.conf
 
-  
-  # Создание флаг-файла
+  # создание флаг-файла
   touch /var/lib/postgresql/data/.init_done
 fi
-
-# Запуск PostgreSQL
-# exec postgres
-
-# делаем бекап
-# pg_basebackup -D /var/lib/postgresql/data/backup -U replicator -v -P --wal-method=stream
-
-# копируем его к себе
-# docker cp socialnetwork-db-master:/var/lib/postgresql/data/backup D:\tmp
-
-# копируем в реплики
-# docker cp D:\tmp\backup socialnetwork-db-replica-1:/var/lib/postgresql/data
-# docker cp D:\tmp\backup socialnetwork-db-replica-2:/var/lib/postgresql/data
